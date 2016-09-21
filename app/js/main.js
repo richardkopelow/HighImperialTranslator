1da03ab20a3017928e69ffbb79839169a3d4c93f
Richard Kopelow <richardkopelow@gmail.com>
2016-09-21 12:56:45 -0400

    Better translation. Added license

--- app/js/main.js
+++ app/js/main.js
@@ -6,10 +6,36 @@ define(function (require, exports, module) {
     var ContainerSurface = require('samsara/dom/ContainerSurface');
     var nlp=require('./nlp_compromise');
 
+    function randomInt(max)
+    {
+        return Math.floor((Math.random() * max));
+    }
+
+    function createPadding(the, frontSpace,backSpace)
+    {
+        the=the==undefined?true:the;
+        frontSpace=frontSpace==undefined?true:frontSpace;
+        backSpace=backSpace==undefined?true:backSpace;
+        var fluff=[
+            'of',
+            'of',
+            'with',
+            'for',
+            'and'
+
+        ]
+        var result=' '+fluff[randomInt(fluff.length)];
+        if (the) {
+            result+=' the';
+        }
+        result+=' ';
+        return result;
+    }
+
     function translate(text)
     {
         var terms=nlp.text(text).terms();
-        var subject=(terms[0].tag=='Person'||terms[0].tag=='Noun')&&terms[0].normal!='i'?terms[0].text:'';
+        var subject=null;//;nlp.text(text).nouns()[0];//(terms[0].tag=='Person'||terms[0].tag=='Noun')&&terms[0].normal!='i'?terms[0].text:'';
         var verb=null;
         var tensePhrase='';
         var theRest='';
@@ -18,18 +44,63 @@ define(function (require, exports, module) {
             var term = terms[i];
             if(verb!=null)
             {
-                theRest+=' '+term.text;
+                if(theRest!=''||!term.pos.Preposition)
+                {
+                    if(term.tag=='Adjective'||term.tag=='Adverb')
+                    {
+                        if(i+1<terms.length)
+                        {
+                            theRest+=' '+terms[i+1].text+createPadding()+term.text;
+                            i++;
+                        }
+                        else
+                        {
+                            theRest+=createPadding()+term.text;
+                        }
+                    }
+                    else
+                    {
+                        theRest+=' '+term.text;
+                    }
+                }
             }
-            if(term.pos.Verb&&verb==null)
+            if(term.pos.Noun&&subject==null)
             {
-                verb=term.text;
-                tensePhrase=term.tag;
+                subject=term;
+                subject=subject.normal=='i'?'':subject.text;
+                if(i-1>-1&&(terms[i-1].tag=='Adjective'||terms[i-1].tag=='Adverb'))
+                {
+                    subject+=createPadding(true,subject!='',false)+terms[i-1].text;
+                }
             }
-            
+            else
+            {
+                if(term.pos.Verb&&verb==null)
+                {
+                    tensePhrase=term.conjugation();
+                    verb=term.text;
+                    if(tensePhrase=='Gerund')
+                    {
+                        var split=verb.split(' ');
+                        if(split.length>1)
+                        {
+                            verb=split[split.length-1];
+                            tensePhrase=term.normal.includes('was')?'PastTense':
+                                        term.normal.includes('am')?'Infinitive':
+                                        'FutureTense'
+                        }
+                    }
+                }
+            }
         }
-        console.log(verb);
-        console.log(nlp.verb(verb).root());
-        verb=nlp.verb(verb).root()+'ing';
+        if(subject==null)
+        {
+            subject='';
+        }
+        var isGerund=nlp.verb(verb).conjugation()=='Gerund';
+        var conjugations=nlp.verb(verb).conjugate();
+        console.log(conjugations);
+        verb=isGerund?conjugations['infinitive']:conjugations['gerund'];
         
         switch(tensePhrase)
         {
@@ -45,9 +116,12 @@ define(function (require, exports, module) {
         }
         if(subject!='')
         {
-            subject=' of the '+subject;
+            subject=createPadding()+subject;
         }
-        verb=' of the '+verb;
+        verb=createPadding()+verb;
+        if (theRest!='') {
+            theRest=createPadding(false)+theRest;
+        }
         return tensePhrase+subject+verb+theRest;
     }
 
